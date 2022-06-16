30cf5a8f06ca7692033400b3619a758186207ae8
Hugo Hashi <hugo_hashimoto@hotmail.com>
2022-06-17 00:32:44 +0100

    day 1

---
+++ day 1/main.py
@@ -0,0 +1,13 @@
+# print
+print('Hello World! \nHello World!')
+
+# input
+name = input('What is your name? \n')
+digits = len(name)
+print(digits)
+
+# Band name generator
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+print("Your band name could be " + city + " " + pet)
